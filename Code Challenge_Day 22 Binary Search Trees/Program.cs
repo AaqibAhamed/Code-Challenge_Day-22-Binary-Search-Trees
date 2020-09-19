1882efef8d2b92fa409fd46fe8d6e1e11dfca5b3
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-19 15:18:20 +0530

    Add project files.

---
+++ Code Challenge_Day 22 Binary Search Trees/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Code_Challenge_Day_22_Binary_Search_Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
