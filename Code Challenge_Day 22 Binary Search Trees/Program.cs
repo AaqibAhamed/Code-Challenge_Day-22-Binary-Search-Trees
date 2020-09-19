66e4007ca9d89e29710482e54f1b141cc209b41e
AaqibGitBit <ahamedaaqib96@gmail.com>
2020-09-19 16:00:58 +0530

    Insert Node

--- Code Challenge_Day 22 Binary Search Trees/Program.cs
+++ Code Challenge_Day 22 Binary Search Trees/Program.cs
@@ -4,9 +4,45 @@ namespace Code_Challenge_Day_22_Binary_Search_Trees
 {
     class Program
     {
-        static void Main(string[] args)
+        static int getHeight(Node root)
         {
-            Console.WriteLine("Hello World!");
+            //Write your code here
+        }
+
+        static Node insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node cur;
+                if (data <= root.data)
+                {
+                    cur = insert(root.left, data);
+                    root.left = cur;
+                }
+                else
+                {
+                    cur = insert(root.right, data);
+                    root.right = cur;
+                }
+                return root;
+            }
+        }
+        static void Main(String[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = insert(root, data);
+            }
+            int height = getHeight(root);
+            Console.WriteLine(height);
+
         }
     }
 }
